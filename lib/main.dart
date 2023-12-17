9f8b383a558b391e3bf19bef92e11864000ad20e
ryozoo <ryozo_ihara@mail.toyota.co.jp>
2023-12-17 09:00:22 +0900

    :+1: #3.7 Icons and Transforms
    https://nomadcoders.co/flutter-for-beginners/lectures/4146

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                   color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(
@@ -128,6 +129,7 @@ class App extends StatelessWidget {
                     30,
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
@@ -165,6 +167,17 @@ class App extends StatelessWidget {
                             ],
                           ),
                         ],
+                      ),
+                      Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-5, 10),
+                          child: const Icon(
+                            Icons.euro_rounded,
+                            color: Colors.white,
+                            size: 88,
+                          ),
+                        ),
                       ),
                     ],
                   ),
