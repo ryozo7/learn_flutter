749f4a9e12f454c2ef0501a6709e44d4d956cd47
ryozoo <ryozo_ihara@mail.toyota.co.jp>
2023-12-17 10:55:31 +0900

    :+1: #4.1 setState
    https://nomadcoders.co/flutter-for-beginners/lectures/4150

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,9 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   int counter = 0;
   void onClicked() {
-    counter = counter + 1;
+    setState(() {
+      counter = counter + 1;
+    });
   }
 
   @override
